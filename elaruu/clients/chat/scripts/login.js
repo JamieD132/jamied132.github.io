10fe90e9b2627a5ee7e7d1549de04db7f33e9989
JamieD132 <160369842+JamieD132@users.noreply.github.com>
2024-11-23 09:16:26 +1100

    Create login.js

---
+++ elaruu/clients/chat/scripts/login.js
@@ -0,0 +1,54 @@
+//login v5.0.0
+//compatable with backend >= v3.0.0
+//compatable with client 2 (chat.jamied132.is-a.dev) using backend 4.1.0
+//variables defined in response from client 2:
+//profileusername: username of the profile currently displayed
+//signedin: true/false
+//_____
+//current migration status:
+//   session storage    -->    local storage   -->   document cookies
+//                             ^^^^^^^^^^^^^
+/*Description
+This script is used within the navigation bar and is returned from topnav(sess)
+it defines the signedin variable
+needs to switch from localstorage to document cookies
+*/
+document.querySelectorAll(".dropdown-toggle").forEach((elem)=>{
+    elem.addEventListener("click",(e)=>{
+        return elem.parentElement.classList.contains("open") ? elem.parentElement.classList.remove("open") : elem.parentElement.classList.add("open");
+    });
+});
+document.querySelector("#login").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    var username = document.querySelector("#login_dropdown_username").value;
+    var password = document.querySelector(".wide.password").value;
+    try{
+    fetch("https://api.jamied132.is-a.dev/auth/signin",{method:"POST",headers:{'Authorization':btoa(JSON.stringify({username:username,password:password}))}}).then(r=>{
+        if (!r.ok) {
+            console.error("Error:", r.status, r.statusText);
+            r.json().then(errorMessage => {
+              console.error("Error message:", errorMessage.message);
+              document.querySelector("#topnav ul.account-nav .sign-in .error").innerHTML = errorMessage.message;
+              document.querySelector("#topnav ul.account-nav .sign-in .error").style.display = "block";
+            }).catch(error => {
+              document.querySelector("#topnav ul.account-nav .sign-in .error").innerHTML = "an unknown error occured. <a href='/contact'>help</a>";
+              document.querySelector("#topnav ul.account-nav .sign-in .error").style.display = "block";
+              console.error("Failed to extract error message:", error);
+            });
+            return Promise.reject(response); // Reject the promise to signal an error
+        }
+        return r.json()
+    }).then(j=>{
+        if(!j.error){
+            fetch("https://api.jamied132.is-a.dev/auth/connect",{method:"POST",headers:{'Authorization':btoa(JSON.stringify({username:username,password:password}))}}).then(r=>r.json()).then(j=>{
+                document.cookie=`session=${j.code || null};`;
+                window.location.reload();
+            })
+            
+        }
+    })
+    }catch(e){
+        document.querySelector("#topnav ul.account-nav .sign-in .error").innerHTML = "an unknown error occured. <a href='/contact'>help</a>";
+        document.querySelector("#topnav ul.account-nav .sign-in .error").style.display = "block";
+    }
+});
