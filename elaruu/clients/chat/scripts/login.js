4deaf923e0c7e362ce898a217c93d3e62f6c7ada
JamieD132 <160369842+JamieD132@users.noreply.github.com>
2024-11-28 17:28:29 +1100

    Update login.js

--- elaruu/clients/chat/scripts/login.js
+++ elaruu/clients/chat/scripts/login.js
@@ -13,50 +13,60 @@ This script is used within the navigation bar and is returned from topnav(sess)
 it defines the signedin variable
 needs to switch from localstorage to document cookies
 */
-document.querySelectorAll(".dropdown-toggle").forEach((elem)=>{
-    elem.addEventListener("click",(e)=>{
-        return elem.parentElement.classList.contains("open") ? elem.parentElement.classList.remove("open") : elem.parentElement.classList.add("open");
+if(document.querySelector(".dropdown-toggle")){
+    document.querySelectorAll(".dropdown-toggle").forEach((elem)=>{
+        elem.addEventListener("click",(e)=>{
+            return elem.parentElement.classList.contains("open") ? elem.parentElement.classList.remove("open") : elem.parentElement.classList.add("open");
+        });
     });
-});
-document.querySelector("#login").addEventListener("submit",(e)=>{
-    e.preventDefault();
-    var username = document.querySelector("#login_dropdown_username").value;
-    var password = document.querySelector(".wide.password").value;
-    try{
-    fetch("https://api.jamied132.is-a.dev/auth/signin",{method:"POST",headers:{'Authorization':btoa(JSON.stringify({username:username,password:password}))}}).then(r=>{
-        if (!r.ok) {
-            console.error("Error:", r.status, r.statusText);
-            r.json().then(errorMessage => {
-              console.error("Error message:", errorMessage.message);
-              document.querySelector("#topnav ul.account-nav .sign-in .error").innerHTML = errorMessage.message;
-              document.querySelector("#topnav ul.account-nav .sign-in .error").style.display = "block";
-            }).catch(error => {
-              document.querySelector("#topnav ul.account-nav .sign-in .error").innerHTML = "an unknown error occured. <a href='/contact'>help</a>";
-              document.querySelector("#topnav ul.account-nav .sign-in .error").style.display = "block";
-              console.error("Failed to extract error message:", error);
-            });
-            return Promise.reject(response); // Reject the promise to signal an error
-        }
-        return r.json()
-    }).then(j=>{
-        if(!j.error){
-            fetch("https://api.jamied132.is-a.dev/auth/connect",{method:"POST",headers:{'Authorization':btoa(JSON.stringify({username:username,password:password}))}}).then(r=>r.json()).then(j=>{
-                document.cookie=`session=${j.code || null};`;
-                window.location.reload();
-            })
-            
-        }
-    })
-    }catch(e){
-        document.querySelector("#topnav ul.account-nav .sign-in .error").innerHTML = "an unknown error occured. <a href='/contact'>help</a>";
-        document.querySelector("#topnav ul.account-nav .sign-in .error").style.display = "block";
-    }
-});
-document.querySelectorAll(".reply").forEach(a=>{
-    a.style.display='inline';
-    a.addEventListener("click",function(e){
+}
+if(document.querySelector("#login")){
+    document.querySelector("#login").addEventListener("submit",(e)=>{
         e.preventDefault();
-        add_reply_form(e.target);
+        var username = document.querySelector("#login_dropdown_username").value;
+        var password = document.querySelector(".wide.password").value;
+        try{
+        fetch("https://api.jamied132.is-a.dev/auth/signin",{method:"POST",headers:{'Authorization':btoa(JSON.stringify({username:username,password:password}))}}).then(r=>{
+            if (!r.ok) {
+                console.error("Error:", r.status, r.statusText);
+                r.json().then(errorMessage => {
+                  console.error("Error message:", errorMessage.message);
+                  document.querySelector("#topnav ul.account-nav .sign-in .error").innerHTML = errorMessage.message;
+                  document.querySelector("#topnav ul.account-nav .sign-in .error").style.display = "block";
+                }).catch(error => {
+                  document.querySelector("#topnav ul.account-nav .sign-in .error").innerHTML = "an unknown error occured. <a href='/contact'>help</a>";
+                  document.querySelector("#topnav ul.account-nav .sign-in .error").style.display = "block";
+                  console.error("Failed to extract error message:", error);
+                });
+                return Promise.reject(response); // Reject the promise to signal an error
+            }
+            return r.json()
+        }).then(j=>{
+            if(!j.error){
+                fetch("https://api.jamied132.is-a.dev/auth/connect",{method:"POST",headers:{'Authorization':btoa(JSON.stringify({username:username,password:password}))}}).then(r=>r.json()).then(j=>{
+                    document.cookie=`session=${j.code || null}; path=/;`;
+                    window.location.reload();
+                })
+                
+            }
+        })
+        }catch(e){
+            document.querySelector("#topnav ul.account-nav .sign-in .error").innerHTML = "an unknown error occured. <a href='/contact'>help</a>";
+            document.querySelector("#topnav ul.account-nav .sign-in .error").style.display = "block";
+        }
     });
-});
-
+}
+if(document.querySelector(".reply")){
+    document.querySelectorAll(".reply").forEach(a=>{
+        a.style.display='inline';
+        a.addEventListener("click",function(e){
+            e.preventDefault();
+            add_reply_form(e.target);
+        });
+    });
+}
+if(document.querySelector("#logout")){
+    document.querySelector("#logout").addEventListener("click",()=>{
+        document.cookie="session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    });
+}
